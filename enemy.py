e07869186491cd8168b82008f158136d03f9ee2e
sabotzs <gkuklev98@gmail.com>
2020-04-03 19:53:03 +0300

    Fix indent in enemy.py

--- enemy.py
+++ enemy.py
@@ -2,7 +2,7 @@ from character import Character
 
 
 class Enemy(Character):
-    def __init__(self, * health, mana, damage):
+    def __init__(self, *, health, mana, damage):
         super().__init__(health=health, mana=mana)
         self.damage = damage
 
